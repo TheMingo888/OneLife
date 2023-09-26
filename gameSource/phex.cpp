926d72874872d2a0f5f9ac0cb5b9e73ddafa7fe2
TheMingo888 <TheMingo888@users.noreply.github.com>
2023-09-26 00:50:26 -0700

    Don't update Phex focus when moving

--- gameSource/phex.cpp
+++ gameSource/phex.cpp
@@ -1174,10 +1174,6 @@ void Phex::onMouseEvent(float x, float y) {
 bool Phex::onMouseDown(float x, float y) {
 	if (!HetuwMod::phexIsEnabled) return false;
 	HetuwMod::pointFromMapToPercentCoords(x, y);
-	if (!HetuwMod::pointIsInsideRec(recBckgr, x, y)) {
-		onUpdateFocus(false);
-		return false;
-	}
 	for(unsigned k=0; k<buttons.size(); k++) {
 		if (buttons[k]->onMouseDown(x, y)) return true;
 	}
